c79cf581bdcf42c3bf8709317a858de02db575c4
Methas Tariya <korstudio@gmail.com>
2022-03-27 21:48:36 +0700

    Add enum Content

---
+++ ResumeBuilder/ResumeBuilder/Models/Content.swift
@@ -0,0 +1,13 @@
+//
+// Content
+// ResumeBuilder
+//
+// Created by Methas Tariya on 21/3/22.
+// Copyright (c) 2022 Methas Tariya. All rights reserved.
+//
+
+import Foundation
+
+enum Content {
+
+}
