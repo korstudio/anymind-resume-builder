ad5bab68d416d25cda5d3cb6cf84dcd348bc5fbd
Methas Tariya <korstudio@gmail.com>
2022-03-27 23:03:02 +0700

    Add StringExtension

---
+++ ResumeBuilder/ResumeBuilder/Extensions/StringExtension.swift
@@ -0,0 +1,24 @@
+//
+// StringExtension
+// ResumeBuilder
+//
+// Created by Methas Tariya on 27/3/22.
+// Copyright (c) 2022 Methas Tariya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    init(instance: Any) {
+        self = String(type: type(of: instance))
+    }
+
+    init(type: Any.Type) {
+        self = String(describing: type)
+    }
+
+    func getImage() -> UIImage? {
+        UIImage(named: self)
+    }
+}
