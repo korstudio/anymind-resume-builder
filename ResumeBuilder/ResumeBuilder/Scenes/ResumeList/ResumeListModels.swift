1c01a58ecc3f3cb89ba8e8779968f99f40122fea
Methas Tariya <korstudio@gmail.com>
2022-04-03 02:54:22 +0700

    Add model for Display flow

--- ResumeBuilder/ResumeBuilder/Scenes/ResumeList/ResumeListModels.swift
+++ ResumeBuilder/ResumeBuilder/Scenes/ResumeList/ResumeListModels.swift
@@ -3,9 +3,26 @@
 //  ResumeBuilder
 //
 //  Created by Methas Tariya on 21/3/22.
-//  Copyright (c) 2022 ___ORGANIZATIONNAME___. All rights reserved.
+//  Copyright (c) 2022 Methas Tariya. All rights reserved.
 //
 
 import UIKit
+import RealmSwift
 
-enum ResumeList {}
+enum ResumeList {
+    struct ResumeContext {
+        let id: ObjectId
+        let title: String
+        let date: Date
+    }
+    
+    enum Display {
+        struct Request {}
+        struct Response {
+            let resumeList: [ResumeContext]
+        }
+        struct ViewModel {
+            let resumeList: [ResumeContext]
+        }
+    }
+}
