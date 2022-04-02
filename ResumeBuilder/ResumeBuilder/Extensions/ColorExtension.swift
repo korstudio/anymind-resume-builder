dacf2bef511757ddf6714d7b28d0bbf6212b16e5
Methas Tariya <korstudio@gmail.com>
2022-04-03 01:05:47 +0700

    Add color extension

---
+++ ResumeBuilder/ResumeBuilder/Extensions/ColorExtension.swift
@@ -0,0 +1,21 @@
+//
+//  ColorExtension.swift
+//  ResumeBuilder
+//
+//  Created by Methas Tariya on 2/4/22.
+//  Copyright © 2022 Methas Tariya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let appYellow = UIColor(red: 1.0, green: 0.878, blue: 0.4, alpha: 1.0) // #ffe066
+    static let appRed = UIColor(red: 0.949, green: 0.373, blue: 0.361, alpha: 1.0) // #f25f5c
+    static let appGreen = UIColor(red: 0.392, green: 0.745, blue: 0.569, alpha: 1.0)// #64be91
+    static let appBlue = UIColor(red: 0.235, green: 0.569, blue: 0.902, alpha: 1.0) // #3c91e6
+    static let text = UIColor(red: 0.204, green: 0.180, blue: 0.216, alpha: 1.0) // #342e37
+    static let secondaryText = UIColor(red: 0.6, green: 0.588, blue: 0.608, alpha: 1.0) // #99969b
+    static let placeholder = UIColor(red: 0.882, green: 0.886, blue: 0.902, alpha: 1.0) // #e1e2e6
+    static let error = UIColor(red: 0.965, green: 0.565, blue: 0.557, alpha: 1.0) // #f6908e
+}
