c83b8fca6ed80974b7bec1fc65ac473c3a5d2565
Methas Tariya <korstudio@gmail.com>
2022-04-10 13:15:15 +0700

    Create typealias for [Field: String]

--- ResumeBuilder/ResumeBuilder/Models/Content.swift
+++ ResumeBuilder/ResumeBuilder/Models/Content.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+typealias FieldMap = [Field: String]
+
 enum Content {
     struct WorkSummaryItem {
         var companyName = ""
@@ -57,5 +59,11 @@ enum Field {
 struct DisplayValue {
     var section: Builder.Section = .info
     var index: Int = 0
-    var content: [Field: String] = [:]
+    var content: FieldMap = [:]
+}
+
+extension FieldMap {
+    func get(_ field: Field) -> String {
+        self[field] ?? ""
+    }
 }
